c709ad1984371a4d92cc82bd8baf25de17eeb777
Diego Ballesteros <yeahimdiego@gmail.com>
2020-06-18 21:33:13 -0500

    Grouperchild skeleton

--- src/components/grouper/grouperChild/GrouperChild.js
+++ src/components/grouper/grouperChild/GrouperChild.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
 const GrouperChild = (props) => (
-    <div>
-        
+    <div className="grouperChild">
+        <a href="/#">
+            <div>
+                <div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <div>
+                    <span>
+                        
+                    </span>
+                </div>
+            </div>
+            <div>
+                <div>
+
+                </div>
+            </div>
+        </a>
     </div>
 );
 
