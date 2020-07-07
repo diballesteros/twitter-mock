2b2a0d9ec3b475714bf257a021d7484f94bd08b5
Diego Ballesteros <yeahimdiego@gmail.com>
2020-07-07 07:36:32 -0500

    Adding apollo for graphql client. Adding query to list component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import Dashboard from './components/dashboard/Dashboard';
 import Login from './components/login/Login';
 import Error from './components/error/Error'
 import { Switch, Route } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
 
 const App = () => {
   return (
-    <div className="container-view">
-      <Switch>
-        <Route path='/' component={Login} exact/>
-        <Route path='/home' component={Dashboard} />
-        <Route component={Error} />
-      </Switch>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container-view">
+        <Switch>
+          <Route path='/' component={Login} exact />
+          <Route path='/home' component={Dashboard} />
+          <Route component={Error} />
+        </Switch>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- src/components/list/List.js
+++ src/components/list/List.js
@@ -2,15 +2,33 @@ import React from 'react';
 import './List.css';
 import Tweet from '../tweet/Tweet';
 import { TWEETCONTENT as tweetContent } from '../../constants/CONSTANTS';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-const List = (props) => (
-    <div className="list">
-        {tweetContent.map((value, i) =>
-            <Tweet
-                key={i}
-                tweetContent={value}>
-            </Tweet>)}
-    </div>
-);
+
+const getTweetsQuery = gql`
+    {
+        tweets{
+            content
+            date
+        }
+    }
+`
+
+const List = (props) => {
+    const { loading, error, data } = useQuery(getTweetsQuery);
+
+    return (
+        <div className="list">
+            {tweetContent.map((value, i) =>
+                <Tweet
+                    key={i}
+                    tweetContent={value}>
+                </Tweet>)}
+        </div>
+    );
+};
+
+
 
 export default List;
