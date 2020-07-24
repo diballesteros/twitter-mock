28963ff63c78a0e5d479d58c6ea7985a92572a3f
Diego Ballesteros <diballesterosc@gmail.com>
2020-07-24 07:49:25 -0500

    Prettier formatting

--- src/helpers/DateHelper.js
+++ src/helpers/DateHelper.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const convertDate = (date) => {
-    const dateDifference = Date.now() - date;
+	const dateDifference = Date.now() - date;
 
-    if (dateDifference > 86400000) {
-        return (new Date(Number(date))).toLocaleString('default', {month: 'long', day: 'numeric'});
-    }
-    if (dateDifference > 3600000) {
-        return (dateDifference / (1000 * 60 * 60)).toFixed() + 'h';
-    }
+	if (dateDifference > 86400000) {
+		return new Date(Number(date)).toLocaleString('default', {
+			month: 'long',
+			day: 'numeric',
+		});
+	}
+	if (dateDifference > 3600000) {
+		return (dateDifference / (1000 * 60 * 60)).toFixed() + 'h';
+	}
 
-    if (dateDifference > 60000) {
-        return (dateDifference / (1000 * 60)).toFixed() + 'm';
-    }
+	if (dateDifference > 60000) {
+		return (dateDifference / (1000 * 60)).toFixed() + 'm';
+	}
 
-    return (dateDifference / 1000).toFixed() + 's';
-}
+	return (dateDifference / 1000).toFixed() + 's';
+};
 
-const DateHelper = ({children}) => (
-        <>{convertDate(children)}</>
-);
+const DateHelper = ({ children }) => <>{convertDate(children)}</>;
 
 export default DateHelper;
